6f1658fe9943b86fcda4bbb5f4cab2fefa84ab86
bharadwaj23456 <79959667+bharadwaj23456@users.noreply.github.com>
2024-08-26 10:53:12 +0530

    Add files via upload

---
+++ tummoc/tummoc/View/CategoriesSheet.swift
@@ -0,0 +1,37 @@
+
+import SwiftUI
+
+struct CategoriesSheet: View {
+    let categories: [Category]
+    @Binding var selectedCategory: Int?
+    @Binding var isSheetPresented: Bool
+
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(categories) { category in
+                    Button(action: {
+                        selectedCategory = category.id
+                        isSheetPresented.toggle()
+                    }) {
+                        Text(category.name)
+                    }
+                }
+            }
+            .navigationTitle("Categories")
+            .navigationBarItems(trailing: Button("Close") {
+                isSheetPresented.toggle()
+            })
+        }
+    }
+}
+#Preview {
+    CategoriesSheet(
+        categories: [
+            Category(id: 1, name: "Category 1", items: []),
+            Category(id: 2, name: "Category 2", items: []),
+        ],
+        selectedCategory: .constant(nil),
+                isSheetPresented: .constant(false)
+    )
+}
